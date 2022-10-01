edd6f92978b9f8c14467b778fc59dcc125aa8f6f
ReimerpJD <cjmacbeth@runbox.com>
2022-10-01 13:25:29 -0400

    dev

--- Server.js
+++ Server.js
@@ -2,9 +2,21 @@ const path=require('path');
 const fs=require('fs');
 const express=require('express');
 const Router=express();
-Router.post('/Terminal/:ID',(Request,Response)=>{});//check password
-Router.post('/User/:ID',(Request,Response)=>{});//check password
-Router.get('/Fixture/:ID',(Request,Response)=>{});//generate challenge
-Router.post('/Fixture/:ID',(Request,Response)=>{});//check challenge
+Router.post('/Terminal/:ID',(Request,Response)=>{
+	console.log('Terminal Login');
+	Response.send(true);
+});//check password
+Router.post('/User/:ID',(Request,Response)=>{
+	console.log('User Login');
+	Response.send(true);
+});//check password
+Router.get('/Fixture/:ID',(Request,Response)=>{
+	console.log('Fixture Challenge Request');
+	Response.send(true);
+});//generate challenge
+Router.post('/Fixture/:ID',(Request,Response)=>{
+	console.log('Fixture Challenge Response');
+	Response.send(true);
+});//check challenge
 Router.post('/Data');//log data under session's fixture, terminal, and user
 module.exports=Router;
